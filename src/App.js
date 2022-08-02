6ba445b9103bd29adb72efc14de4bb7b7751b7d0
Jake Walsh <jkwalsh127@gmail.com>
2022-08-02 00:37:46 -0700

    format header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import '@aws-amplify/ui-react/styles.css';
 import { Authenticator } from '@aws-amplify/ui-react';
 import Agendas from './components/Agendas';
 import { Button, Avatar } from "@mui/material";
-import background from "./images/background.jpg";
+import footerBackground from "./images/footerBackground.jpg";
 
 
 function App() {
@@ -12,14 +12,19 @@ function App() {
   return (
     <Authenticator>
       {({ signOut }) => (
-        <div className='app-wrap' style={{ backgroundImage: `url(${background})`, backgroundSize: "contain", backgroundRepeat: "no-repeat", backgroundPosition: "center bottom" }}>
-          <header className='header'>
-            <Button onClick={signOut} variant="contained" sx={{ mr: 2, height: 25, alignSelf: "center", backgroundColor: "primary.main" }}>Sign Out</Button>
-            <Avatar>R</Avatar>
-          </header>
-
-          <Agendas />
-        </div>
+        <>
+          <div className='app-wrap' style={{ backgroundImage: `url(${footerBackground})`, backgroundSize: "contain", backgroundRepeat: "no-repeat", backgroundPosition: "center bottom" }}>
+            <div className='header-wrap'>
+              <header className='header'>
+                <div className='nav'>
+                  <Button onClick={signOut} variant="contained" sx={{ mr: 2, height: 25, alignSelf: "center", backgroundColor: "primary.main" }}>Sign Out</Button>
+                  <Avatar sx={{ alignSelf: "center" }}>R</Avatar>
+                </div>
+              </header>
+            </div>
+            <Agendas />
+          </div>
+        </>
       )}
     </Authenticator>
   );
--- src/components/Agendas.js
+++ src/components/Agendas.js
@@ -4,12 +4,27 @@ import { API } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { listAgenda } from './../graphql/queries';
 import { createAgenda as createAgendaMutation, deleteAgenda as deleteAgendaMutation } from './../graphql/mutations';
-import { Card, TextField, Button, Typography, Accordion, AccordionSummary } from "@mui/material";
+import { Card, TextField, Button, Typography, Accordion, AccordionSummary, Backdrop, Modal, Box, Fade } from "@mui/material";
 import { ExpandMore } from '@mui/icons-material';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const initialFormState = { title: '', description: '', firsttopic: '', firstestimate: '', secondtopic: '', secondestimate: '', thirdtopic: '', thirdestimate: '', fourthtopic: '', fourthestimate: '' };
 
 export default function Agendas() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const [agendas, setAgendas] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
@@ -38,18 +53,23 @@ export default function Agendas() {
 
   return (
     <Authenticator>
-        <>
-        <Card variant="outlined" sx={{ border: "3px solid #f4673c", maxWidth: 500, minHeight: 400, display: "flex", flexDirection: "column", alignItems: "center" }}>
-          <Typography variant="title">Dashboard</Typography>
-          <Accordion  sx={{ backgroundColor: "#64bfc4", width: "90%" }}>
-            <AccordionSummary
-              expandIcon={<ExpandMore />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography>Add new agenda</Typography>
-            </AccordionSummary>
-            <div>
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+
+          <div>
             <TextField
               sx={{ width: 1 }}
               id="outlined-search"
@@ -114,13 +134,30 @@ export default function Agendas() {
             />
             <Button onClick={createAgenda} variant="outlined" sx={{ color: "customColor.darker" }}>Create Agenda</Button>
             </div>
+
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+        <>
+        <Card variant="outlined" sx={{ border: "3px solid #f4673c", maxWidth: 500, minHeight: 400, display: "flex", flexDirection: "column", alignItems: "center", justifySelf: "center", marginTop: 3, marginBottom: 5, padding: 2, overflow: "scroll" }}>
+          <Typography variant="title">Dashboard</Typography>
+          <Accordion  sx={{ backgroundColor: "#64bfc4", width: "90%" }}>
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Add new agenda</Typography>
+            </AccordionSummary>
+
           </Accordion>
 
-          <div>
-            <h1>Agendas</h1>
+          <div style={{ width: "100%", display: "flex", flexDirection: "column" }}>
+            <h1 style={{ margin: "0" }}>Agendas</h1>
             {
               agendas.map(agenda => (
-                <Accordion  sx={{ backgroundColor: "#64bfc4", width: "90%" }}>
+                <Accordion  sx={{ backgroundColor: "#64bfc4", width: "90%", alignSelf: "center" }}>
                   <AccordionSummary
                     expandIcon={<ExpandMore />}
                     aria-controls="panel2a-content"
@@ -130,7 +167,7 @@ export default function Agendas() {
                       <h2>{agenda.title}</h2>
                     </Typography>
                   </AccordionSummary>
-                  
+
                   <div key={agenda.id || agenda.title}>
                     <p>{agenda.description}</p>
                     <p>{agenda.firsttopic}</p>

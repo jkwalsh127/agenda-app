aac81b5a01f8506e1e9772e47fdf17e24d04fd8b
Jake Walsh <jkwalsh127@gmail.com>
2022-08-03 01:45:27 -0700

    improve formatting of create and edit agenda forms

--- src/components/EditAgenda.js
+++ src/components/EditAgenda.js
@@ -16,7 +16,10 @@ const style = {
   pl: 8,
   pr: 8,
   pt: 2,
-  pb: 2
+  pb: 2,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
 };  
 
 export default function CreateAgenda({ agendas, setAgendas, agendaId, agenda, fetchAgendas }) {
@@ -55,99 +58,124 @@ export default function CreateAgenda({ agendas, setAgendas, agendaId, agenda, fe
       >
         <Fade in={open}>
           <Box sx={style}>
-            <Typography sx={{ m: 'auto', width: 240, fontSize: 28, textDecoration: 'underline #f4673c 3px', borderBottom: '2px solid #f4673c', height: '40px' }}>Add a new agenda</Typography>
+            <Typography sx={{ m: 'auto', width: 182, fontSize: 28, textDecoration: 'underline #f4673c 3px', borderBottom: '2px solid #f4673c', height: '40px' }}>
+                Agenda Editor
+            </Typography>
             <TextField
               sx={{ width: 1, mt: 1 }}
               id="outlined-search"
-              label="Title"
+              label="Agenda Title"
               onChange={e => setFormData({ ...formData, 'title': e.target.value})}
               value={formData.title}
+              size="small"
             />
+            <div>
             <TextField
-              sx={{ width: "65%", mt: 1, ml: "8px" }}
+              sx={{ width: "60%", mt: 1, ml: "8px" }}
               id="outlined-search"
-              label="First Topic"
+              label="1st Topic"
               onChange={e => setFormData({ ...formData, 'firsttopic': e.target.value})}
               value={formData.firsttopic}
+              size="small"
             />
             <TextField
-              sx={{ width: "30%", mt: 1, ml: "10px" }}
+              sx={{ width: "35%", mt: 1, ml: "10px" }}
               id="outlined-search"
               label="time (minutes)"
               onChange={e => setFormData({ ...formData, 'firstestimate': e.target.value})}
               value={formData.firstestimate}
+              size="small"
             />
+            </div>
             <TextField
               sx={{ width: 1, mt: 1 }}
               id="outlined-search"
-              label="Description"
+              label="1st Description"
               onChange={e => setFormData({ ...formData, 'firstdescription': e.target.value})}
               value={formData.firstdescription}
+              size="small"
             />
+            <div>
             <TextField
-              sx={{ width: "65%", mt: 1, ml: "8px" }}
+              sx={{ width: "60%", mt: 1, ml: "8px" }}
               id="outlined-search"
-              label="Second Topic"
+              label="2nd Topic"
               onChange={e => setFormData({ ...formData, 'secondtopic': e.target.value})}
               value={formData.secondtopic}
+              size="small"
             />
             <TextField
-              sx={{ width: "30%", mt: 1, ml: "10px" }}
+              sx={{ width: "35%", mt: 1, ml: "10px" }}
               id="outlined-search"
               label="time (minutes)"
               onChange={e => setFormData({ ...formData, 'secondestimate': e.target.value})}
               value={formData.secondestimate}
+              size="small"
             />
+            </div>
             <TextField
               sx={{ width: 1, mt: 1 }}
               id="outlined-search"
-              label="Description"
+              label="2nd Description"
               onChange={e => setFormData({ ...formData, 'seconddescription': e.target.value})}
               value={formData.seconddescription}
+              size="small"
             />
+            <div>
             <TextField
-              sx={{ width: "65%", mt: 1, ml: "8px" }}
+              sx={{ width: "60%", mt: 1, ml: "8px" }}
               id="outlined-search"
-              label="Third Topic"
+              label="3rd Topic"
               onChange={e => setFormData({ ...formData, 'thirdtopic': e.target.value})}
               value={formData.thirdtopic}
+              size="small"
             />
             <TextField
-              sx={{ width: "30%", mt: 1, ml: "10px" }}
+              sx={{ width: "35%", mt: 1, ml: "10px" }}
               id="outlined-search"
               label="time (minutes)"
               onChange={e => setFormData({ ...formData, 'thirdestimate': e.target.value})}
               value={formData.thirdestimate}
+              size="small"
             />
+            </div>
             <TextField
               sx={{ width: 1, mt: 1 }}
               id="outlined-search"
-              label="Description"
+              label="3rd Description"
               onChange={e => setFormData({ ...formData, 'thirddescription': e.target.value})}
               value={formData.thirddescription}
+              size="small"
             />
+            <div>
             <TextField
-              sx={{ width: "65%", mt: 1, ml: "8px" }}
+              sx={{ width: "60%", mt: 1, ml: "8px" }}
               id="outlined-search"
-              label="Fourth Topic"
+              label="4th Topic"
               onChange={e => setFormData({ ...formData, 'fourthtopic': e.target.value})}
               value={formData.fourthtopic}
+              size="small"
             />
             <TextField
-              sx={{ width: "30%", mt: 1, ml: "10px" }}
+              sx={{ width: "35%", mt: 1, ml: "10px" }}
               id="outlined-search"
               label="time (minutes)"
               onChange={e => setFormData({ ...formData, 'fourthestimate': e.target.value})}
               value={formData.fourthestimate}
+              size="small"
             />
+            </div>
             <TextField
               sx={{ width: 1, mt: 1 }}
               id="outlined-search"
-              label="Description"
+              label="4th Description"
               onChange={e => setFormData({ ...formData, 'fourthdescription': e.target.value})}
               value={formData.fourthdescription}
+              size="small"
             />
-            <Button onClick={updateAgenda} variant="contained" sx={{ color: "#fffaf6", backgroundColor: "#11717d", mt: '10px', ml: "90px", width: "170px" }}>Update Agenda</Button>
+            <Button onClick={updateAgenda} variant="contained" sx={{ color: "#fffaf6", backgroundColor: "#11717d", mt: '10px', width: "170px" }}>
+                Update Agenda
+            </Button>
           </Box>
         </Fade>
       </Modal>
